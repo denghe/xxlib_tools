e910cefb2d370cc3b0a7fa995fe9624e9522c4ce
denghe <denghe@hotmail.com>
2020-07-24 18:57:55 +0800

    writing...

--- FishEditor/FishManage.cs
+++ FishEditor/FishManage.cs
@@ -1,4 +1,5 @@
 #pragma warning disable 0169, 0414
+using System;
 using TemplateLibrary;
 
 /*
@@ -14,7 +15,7 @@ using TemplateLibrary;
 	4级: 需要通过 1 & 2 级 资源组合而来，例如 组合
 	5级：间接资源，例如 plist / atlas / c3b 引用到的贴图文件
 
-1 ~ 3 级资源，都需要不同程度的附加一些 跑逻辑所需数据
+1 ~ 4 级资源，都需要不同程度的附加一些 跑逻辑所需数据
 
 具体结构：
 	声音 (附加播放时长)
@@ -23,7 +24,7 @@ using TemplateLibrary;
 		音效
 			mp3, ogg, wav
 
-	图片/纹理/贴图/材质 (附加 碰撞检测 & 锁定数据)
+	单图( 附加 碰撞检测 & 锁定数据 )
 		pkm, webp, png
 
 	图集
@@ -34,13 +35,13 @@ using TemplateLibrary;
 	动画 (附加 动作列表: 名字 & 时长 & 宽高 & 帧率 & 每帧的 碰撞检测 & 锁定数据)
 		2d骨骼
 			spine( atlas, json )
-				纹理引用到 图片
+				纹理引用到 单图
 		3d
 			c3b
-				纹理引用到 图片
+				纹理引用到 单图
 		序列帧
 			配置
-				引用到一组 图片 或 小图块
+				引用到一组 单图 或 小图块( 通过单图的附加信息映射 )
 		组合
 			配置
 				引用到一组 动画(含别的组合)
@@ -49,44 +50,48 @@ using TemplateLibrary;
 
 其中 序列帧, 组合 比较特殊，cocos 并未内置相关物理存储结构，故将其直接创建到 配置 当中
 
+考虑到 plist 属于动态内容，所有附加配置都不基于它，工具只通过单图 路径/文件名 和 plist 里的图块 建立使用映射关系
+即单图资源 附加一个指向所在 plist 文件的配置, 该配置自动生成。要求 图块名和单图 路径/文件名 完全对应
+
+
+考虑到多语言的情况，所有资源和文件的对应，均通过 字典 来实现一对多。代码中只针对 资源名，不触碰物理文件名
+
 */
 
 
 
 [Desc("整套配置的数据存档. 鱼 = 资源数据 + 逻辑数据")]
 struct Data {
-	[Desc("所有文件列表. key 为 文件名(带部分路径)")]
+	[Desc("所有 文件 列表. key 为 文件名(带部分路径)")]
 	Dict<string, Shared<File>> files;
 
-	[Desc("所有背景音乐资源列表")]
+	[Desc("所有 背景音乐 资源列表")]
 	Dict<string, Shared<ResMusic>> resMusics;
-	[Desc("所有音效资源列表")]
+	[Desc("所有 音效 资源列表")]
 	Dict<string, Shared<ResVoice>> resVoices;
 
-	[Desc("所有纹理资源列表")]
-	Dict<string, Shared<ResTexture>> resTextures;
-
-	[Desc("所有图集资源列表")]
+	[Desc("所有 单图 资源列表")]
+	Dict<string, Shared<ResPicture>> resTextures;
+	[Desc("所有 图集 资源列表")]
 	Dict<string, Shared<ResPList>> resPLists;
-	[Desc("所有图块资源列表( 从 resPlists 汇集 ). key 为 name")]
-	Dict<string, Weak<ResFrame>> resFrames;
 
-	[Desc("所有2d骨骼动画资源列表")]
+	[Desc("所有 2d骨骼动画 资源列表")]
 	Dict<string, Shared<ResSpine>> resSpines;
-	[Desc("所有3d模型动画资源列表")]
+	[Desc("所有 3d模型动画 资源列表")]
 	Dict<string, Shared<Res3d>> res3ds;
-	[Desc("所有帧动画资源列表")]
+	[Desc("所有 帧动画 资源列表")]
 	Dict<string, Shared<ResFrameAnimation>> resFrameAnimations;
 
-	[Desc("所有组合资源列表")]
+	[Desc("所有 组合 资源列表")]
 	Dict<string, Shared<ResCombine>> resCombine;
 
-	[Desc("所有脚本资源列表")]
+	[Desc("所有 脚本 资源列表")]
 	Dict<string, Shared<ResScript>> resScripts;
 
-	[Desc("所有鱼列表")]
+	[Desc("所有 鱼 列表")]
 	Dict<string, Shared<FishBase>> fishs;
 }
+
 
 [Desc("文件扩展名枚举")]
 enum FileExtensions {
@@ -103,27 +108,46 @@ enum FileExtensions {
 class File {
 	[Desc("文件名: 相对路径, 目录名 + 文件名 + 扩展名，前面不带'/'")]
 	string path;
+
 	[Desc("文件扩展名 / 类型")]
 	FileExtensions ext;
+
 	[Desc("文件长度")]
 	long length;
+
 	[Desc("校验码")]
 	byte[] md5;
-	// more?
+
+	[Desc("附带文件集( 例如 plist 附带 texture, atlas 附带 json & texture 等 ). 不被 资源 直接引用")]
+	List<Shared<File>> childs;
+
+	[Desc("如果是图片，和某 plist 的图块 path == name，则指向该 plist. 运行时如果缺单图可从 plist 加载")]
+	Weak<File> mapTo;	// todo: 需要考虑语言因素。不同语言 plist 可能不同
 }
+
+
+[Struct, Desc("属于特定语言的文件描述")]
+class LangFile {
+	[Desc("语言id. 0 代表通用")]
+	int langId;
+
+	[Desc("指向文件")]
+	Weak<File> file;
+}
+
 
 [TypeId(2), Desc("资源基类")]
 class ResBase {
-	[Desc("资源名( 首次创建时直接以文件名作为资源名使用, 故文件名最好跨目录跨图集打包不重名 )")]
+	[Desc("资源名( 初创时以 path+文件名 为资源名 )")]
 	string name;
+
+	[Desc("对应到语言&文件. 至少有一条 语言id == 0 的数据. ")]
+	List<LangFile> files;
 }
 
 
 [TypeId(3), Desc("声音类资源")]
 class ResSound : ResBase {
-	[Desc("对应哪个物理文件")]
-	Weak<File> file;
-
 	[Desc("播放时长")]
 	float seconds;
 }
@@ -132,22 +156,10 @@ class ResSound : ResBase {
 class ResMusic : ResSound {
 }
 
-
 [TypeId(5), Desc("声音/音效类资源")]
 class ResVoice : ResSound {
 }
 
-[TypeId(6), Desc("图集类资源")]
-class ResPList : ResBase {
-	[Desc("对应哪个物理文件")]
-	Weak<File> file;
-
-	[Desc("引用到的纹理资源")]
-	Weak<ResTexture> texture;
-
-	[Desc("含有的帧图资源集合")]
-	List<Shared<ResFrame>> frames;
-}
 
 [TypeId(7), Desc("图片类资源")]
 class ResPicture : ResBase {
@@ -155,17 +167,12 @@ class ResPicture : ResBase {
 	CDCirclesLockPoints cdclps;
 }
 
-[TypeId(8), Desc("图片/纹理类资源")]
-class ResTexture : ResPicture {
-	[Desc("对应哪个物理文件")]
-	Weak<File> file;
+[TypeId(6), Desc("图集类资源( 无法直接使用的资源. 只用来查找对单图和图块的应关系 )")]
+class ResPList : ResBase {
+	[Desc("含有的帧图资源集合")]
+	List<string> frames;
 }
 
-[TypeId(9), Desc("图片/图块类资源")]
-class ResFrame : ResPicture {
-	[Desc("所在 plist 资源")]
-	Weak<ResPList> owner;
-}
 
 [TypeId(10), Desc("动画类资源")]
 class ResAnimation : ResBase {
@@ -179,32 +186,17 @@ class ResAnimation : ResBase {
 
 [TypeId(11), Desc("动画/Spine类资源")]
 class ResSpine : ResAnimation {
-	[Desc("对应 atlas 文件")]
-	Weak<File> atlasFile;
-
-	[Desc("引用到的贴图集合")]
-	List<Weak<ResTexture>> textures;
-
-	[Desc("对应 json 文件")]
-	Weak<File> jsonFile;
-
 	[Desc("含有的动作列表")]
 	List<ActionOthers> actions;
 }
 
 [TypeId(12), Desc("动画/3d类资源")]
 class Res3d : ResAnimation {
-	[Desc("对应 c3b 文件")]
-	Weak<File> file;
-
-	[Desc("引用到的贴图集合")]
-	List<Weak<ResTexture>> textures;
-
 	[Desc("含有的动作列表")]
 	List<ActionOthers> actions;
 }
 
-[TypeId(13), Desc("动画/序列帧类资源. actions 的成员为 ActionSpriteFrame 类型")]
+[TypeId(13), Desc("动画/序列帧类资源")]
 class ResFrameAnimation : ResAnimation {
 	[Desc("含有的动作列表")]
 	List<ActionSpriteFrame> actions;
@@ -216,11 +208,8 @@ class ResCombine : ResAnimation {
 	List<CombineItem> items;
 }
 
-[TypeId(22), Desc("脚本类资源")]
+[TypeId(8), Desc("脚本类资源")]
 class ResScript : ResBase {
-	[Desc("对应哪个物理文件")]
-	Weak<File> file;
-
 	// todo: 功能？作用？接口？存储？
 }
 
@@ -228,6 +217,8 @@ class ResScript : ResBase {
 
 [Struct, Desc("ResCombine 的专用 成员配置")]
 class CombineItem {
+	[Desc("指向要用到的资源名")]
+	string resName;
 	[Desc("指向要用到的资源")]
 	Weak<ResAnimation> res;
 
@@ -264,6 +255,8 @@ class ActionBase {
 class ActionSpriteFrame : ActionBase {
 	[Desc("帧集合")]
 	List<Weak<ResPicture>> frames;
+	[Desc("帧名集合")]
+	List<string> frameNames;
 }
 
 [Struct, Desc("非序列帧的动作：直接附加 碰撞检测圆 + 锁定点线 集合 的 集合( 对应每一帧的数据)")]
@@ -317,9 +310,13 @@ class FishBase {
 
 	[Desc("引用到声音资源( 通常死亡的时候有一定概率播放 ). 可空")]
 	Weak<ResVoice> resVoiceForDead;
+	[Desc("引用到声音资源名")]
+	string resVoiceNameForDead;
 
 	[Desc("引用到动画资源( 通常含有两个动作：idle/move, die/dead ). 必填")]
 	Weak<ResAnimation> resAnimation;
+	[Desc("引用到动画资源名")]
+	string resAnimationName;
 
 	[Desc("鱼状态列表( 初始为第一个状态 ). 必填")]
 	List<FishState> fishStates;
@@ -329,6 +326,8 @@ class FishBase {
 
 	[Desc("引用到脚本资源（不填则表示不用）. 可空")]
 	Weak<ResScript> resScript;
+	[Desc("引用到脚本资源名")]
+	string resScriptName;
 
 	[Desc("倍率基数")]
 	long coin1;
@@ -387,7 +386,7 @@ class FishCyclone : FishBase {
 	string sameFishName;
 }
 
-[TypeId(21), Desc("捕食者( 李逵啥的，接近别的鱼会吞噬对方，倍率相应增加 )")]
+[TypeId(9), Desc("捕食者( 李逵啥的，接近别的鱼会吞噬对方，倍率相应增加 )")]
 class FishEater : FishBase {
 	[Desc("允许被吃掉的鱼的名称列表")]
 	List<string> fishNames;
